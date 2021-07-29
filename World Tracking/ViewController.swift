aa906273e4ffae61c9ef0fda2b56788b00934b7f
Юлия <yuliya.karanevskaya@gmail.com>
2021-07-29 14:16:40 +0300

    initial commit

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    let sceneView: ARSCNView = {
+        let view = ARSCNView()
+        return view
+    }()
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(sceneView)
+        self.sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
+        self.sceneView.session.run(configuration)
+       
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        sceneView.frame = view.bounds
     }
 
 

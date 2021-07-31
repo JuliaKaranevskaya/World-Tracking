75dd20f37243d68c86f27a608e4195beeb46e661
Юлия <yuliya.karanevskaya@gmail.com>
2021-07-31 16:25:39 +0300

    3D house object

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -54,25 +54,41 @@ class ViewController: UIViewController {
     }
     
     @objc private func addAction() {
-        let node = SCNNode()
+        
         //node.geometry = SCNTube(innerRadius: 0.1, outerRadius: 0.2, height: 0.2)
         //node.geometry = SCNSphere(radius: 0.1)
         //node.geometry = SCNCone(topRadius: 0, bottomRadius: 0.2, height: 0.2)
         //node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
         //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
         
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: 0, y: 0.2))
-        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
-        node.geometry = SCNShape(path: path, extrusionDepth: 0.2)
-        node.geometry?.firstMaterial?.specular.contents = UIColor.white
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x: 0, y: 0))
+//        path.addLine(to: CGPoint(x: 0, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+//        node.geometry = SCNShape(path: path, extrusionDepth: 0.2)
+//        let cylinderNode = SCNNode(geometry: SCNCylinder(radius: 0.05, height: 0.1))
+//        cylinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        
+        
+        let roofNode = SCNNode()
+        roofNode.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        roofNode.geometry?.firstMaterial?.specular.contents = UIColor.white
+        roofNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
 //        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
 //        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
 //        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(0, 0, -0.3)
-        sceneView.scene.rootNode.addChildNode(node)
+        roofNode.position = SCNVector3(0.2, 0.3, -0.2)
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0, -0.02, 0.055)
+        sceneView.scene.rootNode.addChildNode(roofNode)
+        roofNode.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+        //sceneView.scene.rootNode.addChildNode(cylinderNode)
     }
     
     @objc private func resetAction() {
